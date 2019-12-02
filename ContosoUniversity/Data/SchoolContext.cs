5d062a5f816650f85fb39a9e73963f41ea1f2fed
kellycoles <kellycoles1@gmail.com>
2019-12-01 19:48:01 -0600

    Add project files.

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    namespace ContosoUniversity.Data
+    {
+        public class SchoolContext : DbContext
+        {
+            public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+            {
+            }
+
+            public DbSet<Course> Courses { get; set; }
+            public DbSet<Enrollment> Enrollments { get; set; }
+            public DbSet<Student> Students { get; set; }
+
+            // Make database tables singular
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Course>().ToTable("Course");
+                modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+                modelBuilder.Entity<Student>().ToTable("Student");
+
+                //seed database
+
+            }
+
+        }
+    }
+}
